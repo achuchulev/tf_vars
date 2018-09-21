16103485acca46c44fae9b64ec789fb5e2619e33
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-09-21 10:02:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 module "my-module" {
   tag_name               = "${var.tag_name}"
-  source                 = "./module1"
+  source                 = "./module"
   ami                    = "${var.ami}"
   instance_type          = "${var.instance_type}"
   subnet_id              = "${var.subnet_id}"
