afe23939909176288b944ff936b7dd69f8206468
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-09-21 10:24:42 +0300

    Update server.tf

--- module/server.tf
+++ module/server.tf
@@ -3,14 +3,9 @@ resource "aws_instance" "atanasc-w1" {
   instance_type          = "${var.instance_type}"
   subnet_id              = "${var.subnet_id}"
   vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
-  key_name               = "${aws_key_pair.my_key_pair.id}"
+  key_name               = "${var.key_pair}"
 
   tags {
     Name = "${var.tag_name}"
   }
-}
-
-resource "aws_key_pair" "my_key_pair" {
-  key_name   = "${var.identity}-key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
